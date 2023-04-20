b3ad74d99da274b1e4c12715de3016c8228b8dda
vjsmit <vj.smit97@gmail.com>
2023-04-21 00:15:37 +0530

    Create Frontend Script & some config files

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,5 @@
 yum install nginx -y
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
