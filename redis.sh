db262301de4985778ccbf49f45fd849e93ae22dd
vjsmit <vj.smit97@gmail.com>
2023-04-22 16:24:15 +0530

    Configuring catalogue service

--- redis.sh
+++ redis.sh
@@ -1,16 +1,16 @@
-echo -e "\e[31m>>>>>>>>>>>>Installing Redis repo file<<<<<<<<<<<<<<<<<[0m"
+echo -e "\e[31m>>>>>>>>>>>>Installing Redis repo file<<<<<<<<<<<<<<<<<\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
-echo -e "\e[31m>>>>>>>>>>>>Enable Redis 6.2<<<<<<<<<<<<<<<<<[0m"
+echo -e "\e[31m>>>>>>>>>>>>Enable Redis 6.2<<<<<<<<<<<<<<<<<\e[0m"
 dnf module enable redis:remi-6.2 -y
 
-echo -e "\e[31m>>>>>>>>>>>>Install Redis<<<<<<<<<<<<<<<<[0m"
+echo -e "\e[31m>>>>>>>>>>>>Install Redis<<<<<<<<<<<<<<<<\e[0m"
 yum install redis -y
 
-echo -e "\e[31m>>>>>>>>>>>>Update redis listen address<<<<<<<<<<<<<<<<[0m"
+echo -e "\e[31m>>>>>>>>>>>>Update redis listen address<<<<<<<<<<<<<<<<\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
 
-echo -e "\e[31m>>>>>>>>>>>>Start & Enable Redis Service<<<<<<<<<<<<<<<<<[0m"
+echo -e "\e[31m>>>>>>>>>>>>Start & Enable Redis Service<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable redis
 systemctl start redis
