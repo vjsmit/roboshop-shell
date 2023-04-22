327d6e7f674168c750be7a5f6c6a0aee4186b5d5
vjsmit <vj.smit97@gmail.com>
2023-04-22 18:34:17 +0530

    configure user service

---
+++ cart.sh
@@ -0,0 +1,31 @@
+echo -e "\e[31m>>>>>>>>>>>Setup NodeJS repo<<<<<<<<<<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[31m>>>>>>>>>>>Install NodeJS<<<<<<<<<<<<<<\e[0m"
+yum install nodejs -y
+
+echo -e "\e[31m>>>>>>>>>>>Add app User<<<<<<<<<<<<<<\e[0m"
+useradd roboshop
+
+echo -e "\e[31m>>>>>>>>>>>setup an app directory<<<<<<<<<<<<<<\e[0m"
+rm -rf /app
+mkdir /app
+echo -e "\e[31mOk\e[0m"
+
+echo -e "\e[31m>>>>>>>>>>>Download the app code<<<<<<<<<<<<<<\e[0m"
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+cd /app
+unzip /tmp/cart.zip
+
+echo -e "\e[31m>>>>>>>>>>>download the dependencies<<<<<<<<<<<<<<\e[0m"
+npm install
+
+echo -e "\e[31m>>>>>>>>>>>Copy SystemD User Service<<<<<<<<<<<<<<\e[0m"
+cp /root/roboshop-shell/cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[31m>>>>>>>>>>>Load the service<<<<<<<<<<<<<<\e[0m"
+systemctl daemon-reload
+systemctl enable cart
+systemctl start cart
+
+
