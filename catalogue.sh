321251b844d8f78f1b5995392c7d4060c0824393
vjsmit <vj.smit97@gmail.com>
2023-04-22 13:26:59 +0530

    Configuring catalogue service

--- catalogue.sh
+++ catalogue.sh
@@ -31,7 +31,7 @@ systemctl enable catalogue
 systemctl start catalogue
 
 echo -e "\e[31m>>>>>>>>>>>>>Copy MongoDB repo<<<<<<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /root/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[31m>>>>>>>>>>>>>Install mongodb-client<<<<<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
