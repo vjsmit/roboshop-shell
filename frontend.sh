87fd5ac32f4e5edf138fe25fd210fddf45de0af3
vjsmit <vj.smit97@gmail.com>
2023-04-22 14:06:06 +0530

    Configuring catalogue service

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
-echo -e "\e[36m>>>>>>>>>>>>>>>Install Nginx<<<<<<<<<<<<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>>>>>>Install Nginx<<<<<<<<<<<<<<<<<<<<<<"
 yum install nginx -y
 
-echo -e "\e[36m>>>>>>>>>>>>>>>Copy Nginx Reverse Proxy file<<<<<<<<<<<<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>>>>>>Copy Nginx Reverse Proxy file<<<<<<<<<<<<<<<<<<<<<<"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[36m>>>>>>>>>>>>>>>Remove the default content that web server is serving<<<<<<<<<<<<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>>>>>>Remove the default content that web server is serving<<<<<<<<<<<<<<<<<<<<<<"
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[36m>>>>>>>>>>>>>>>Download the frontend content<<<<<<<<<<<<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>>>>>>Download the frontend content<<<<<<<<<<<<<<<<<<<<<<"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[36m>>>>>>>>>>>>>>>Extract the frontend content<<<<<<<<<<<<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>>>>>>Extract the frontend content<<<<<<<<<<<<<<<<<<<<<<"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-echo -e "\e[36m>>>>>>>>>>>>>>>Restart Nginx Service<<<<<<<<<<<<<<<<<<<<<<"
+echo -e "\e[34m>>>>>>>>>>>>>>>Restart Nginx Service<<<<<<<<<<<<<<<<<<<<<<"
 systemctl enable nginx
 systemctl restart nginx
